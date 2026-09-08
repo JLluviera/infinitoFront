fb1e9629bfa57af85d6328ff6279431c38b312a7
JLluviera <juanlluviera03@gmail.com>
2026-09-08 20:07:28 -0300

    Componente alertas globales, se muestran sobre toda la app y se pueden llamar desde cualquier lado gracias al AlertService. Se agrega Alert Service que permite manejar las alertas de manera sencilla desde cualquier parte la la aplicacion, incluso manejar confirmaciones de eventos.

---
+++ src/app/components/alertas/alertas-globales.component/alertas-globales.component.ts
@@ -0,0 +1,36 @@
+import { Component, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AlertService, Alerta } from '../../../services/alert.service/alert-service';
+
+@Component({
+  selector: 'app-alertas-globales',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './alertas-globales.component.html',
+  host: {
+    'style': 'display: contents;'
+  },
+  styleUrl: './alertas-globales.component.css'
+  
+})
+export class AlertasGlobalesComponent {
+  alertService = inject(AlertService);
+
+  resolveConfirm(alert: Alerta, result: boolean) {
+    if (alert.resolve) {
+      alert.resolve(result);
+    }
+    this.alertService.removeAlert(alert.id);
+  }
+
+  getAlertStyle(type: string): string {
+    const base = "dark:bg-slate-800 bg-white dark:border-slate-700 border-gray-100 dark:text-white text-gray-800 ";
+    switch (type) {
+      case 'success': return base + 'border-l-4 border-l-green-500';
+      case 'error': return base + 'border-l-4 border-l-red-500';
+      case 'warning': return base + 'border-l-4 border-l-yellow-500';
+      case 'confirm': return base + 'border-l-4 border-l-blue-500 ring-4 ring-black/5 dark:ring-white/5';
+      default: return base + 'border-l-4 border-l-gray-400';
+    }
+  }
+}
--- src/app/pages/layout.component/layout.component.ts
+++ src/app/pages/layout.component/layout.component.ts
@@ -1,15 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { HeaderComponent } from '../../components/header.component/header.component';
 import { SidebarComponent } from '../../components/sidebar.component/sidebar.component';
 import { RouterOutlet } from '@angular/router';
+import { AlertasGlobalesComponent } from '../../components/alertas/alertas-globales.component/alertas-globales.component'
+import { AlertService } from '../../services/alert.service/alert-service';
 
 @Component({
   selector: 'app-layout.component',
-  imports: [HeaderComponent, SidebarComponent, RouterOutlet],
+  imports: [HeaderComponent, SidebarComponent, RouterOutlet, AlertasGlobalesComponent],
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.css',
 })
 export class LayoutComponent {
     isSidebarOpen: boolean = false;
 
-}
+    private alertService = inject(AlertService);
+
+};
